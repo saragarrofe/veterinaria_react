0bc265d098a7debcb6dafa95ffdf8b0f39e3129b
Sara <s.garrofe@gmail.com>
2023-07-21 10:39:23 +0200

    He cambiado el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
     return(
         <>
             <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-                Seguimiento de pacientes {' '}
+                Seguimiento de pacientes de {' '}
                 <span className="text-indigo-600">Veterinaria</span>
             </h1>
         </>
